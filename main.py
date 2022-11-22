3024dbbb774bdf3d4eb87e586fd4586bb100291a
tob wot <76918816+tobywatts@users.noreply.github.com>
2022-11-22 11:55:46 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ import pygame
 
 
 pygame.init()
+
+screen_width = 1000
+screen_height = 700  
+win = pygame.display.set_mode((screen_width, screen_height))
+
+running = True
+
+while running:
+  for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False 
+            pygame.quit()
+            quit()
